1b7379e56c8c07bbe3e8e598fef685b526235c6a
Ronald Maciel <ronaldsmaciel2@outlook.com>
2024-09-28 20:37:18 -0300

    basic struct of tableview

--- BookStore/BookStore/ViewControllers/BookDetailViewController.swift
+++ BookStore/BookStore/ViewControllers/BookDetailViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class BookDetailViewController: UIViewController {
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- BookStore/BookStore/ViewControllers/BooksTableViewController.swift
+++ BookStore/BookStore/ViewControllers/BooksTableViewController.swift
@@ -7,9 +7,9 @@
 
 import UIKit
 
-final class BooksTableViewController: UITableViewController {
-    private var books = [BookListResponse]()
 
+final class BooksTableViewController: UITableViewController {
+    private var books = ["BOOK 1", "BOOK 2", "BOOK 3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +20,32 @@ final class BooksTableViewController: UITableViewController {
     private func setupTableView() {
         tableView.dataSource = self
         tableView.delegate = self
+        tableView?.rowHeight = 200
+        tableView?.estimatedRowHeight = 200
     }
 
 }
+ 
+extension BooksTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let book = books[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: BookCell.identifier) as? BookCell else { return UITableViewCell() }
+        
+        // cell.configure(with: book)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "BookCell", sender: self)
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! BookDetailViewController
+    }
+
+}
 
