a4f7463af7235a44b4130d78240ff1baa632926b
Ronald Maciel <ronaldsmaciel2@outlook.com>
2024-09-28 18:04:41 -0300

    start to create the backbone of the project, add model

---
+++ BookStore/BookStore/ViewControllers/BookDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BookDetailViewController.swift
+//  BookStore
+//
+//  Created by Ronald on 28/09/24.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ BookStore/BookStore/ViewControllers/BooksViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BookStore
+//
+//  Created by Ronald on 28/09/24.
+//
+
+import UIKit
+
+class BooksViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
